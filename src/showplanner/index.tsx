8993f7370957e7964a21f497255cb21ee4f8aa85
thekingofjibs <allen-b@outlook.com>
2020-03-18 17:06:49 +0000

    logo coz logo, also i hate the default scroll bars

--- src/showplanner/index.tsx
+++ src/showplanner/index.tsx
@@ -275,7 +275,10 @@ const Showplanner: React.FC<{ timeslotId: number }> = function({ timeslotId }) {
   }
   return (
     <div className="sp-container">
-      <h1>baps3 ayy lmao</h1>
+      <div style={{height:"10%"}}>
+        <h1>baps3 ayy lmao</h1>
+        <img src="https://ury.org.uk/images/logo.png" style={{height:"6%", right:"2%", position:"absolute", top:"2%"}}/>
+      </div>
       <div className="sp-status">
         {planSaving && <em>Plan saving...</em>}
         {planSaveError && (
