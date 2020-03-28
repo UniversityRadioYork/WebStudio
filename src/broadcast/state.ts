b0261386c5351a1172d9f84490c554037de886df
Matthew Stratford <hi@mstratford.net>
2020-03-28 15:44:59 +0000

    Fix API tracklisting URL's

--- src/broadcast/state.ts
+++ src/broadcast/state.ts
@@ -40,13 +40,13 @@ export const toggleTracklisting = (): AppThunk => dispatch => {
 export const tracklistStart = (trackid: number): AppThunk =>(dispatch, getState) => {
   console.log("Attempting to tracklist: " + trackid);
   if (getState().broadcast.tracklisting) {
-    return myradioApiRequest("/TracklistItem", "POST", { trackid: trackid, source: 'w', state: 'c' });
+    return myradioApiRequest("/tracklistItem", "POST", { trackid: trackid, source: 'w', state: 'c' });
   }
 };
 
 export const tracklistEnd = (tracklistitemid: number): AppThunk => (dispatch, getState) => {
   console.log("Attempting to end tracklistitem: " + tracklistitemid);
   if (getState().broadcast.tracklisting) {
-    myradioApiRequest("/TracklistItem/" + tracklistitemid + "/endtime", "PUT", { });
+    myradioApiRequest("/tracklistItem/" + tracklistitemid + "/endtime", "PUT", { });
   }
 };
