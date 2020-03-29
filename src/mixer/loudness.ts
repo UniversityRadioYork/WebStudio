4ce4a9a4fe2337d6e2eed3b118ec3100a9c382f5
Marks Polakovs <github@markspolakovs.me>
2020-03-29 17:22:44 +0200

    Shitty attempt at sorting out peak calculation

--- src/mixer/loudness.ts
+++ src/mixer/loudness.ts
@@ -5,17 +5,16 @@ export async function createLoudnessMeasurement(
 	input: AudioNode,
 	callback: (data: { peak: number, loudness: number }) => any
 ) {
-	console.log(input)
-	const shelf = input.context.createBiquadFilter();
-	shelf.type = "highshelf";
-	shelf.frequency.value = 1500;
-	shelf.gain.value = 4;
-	input.connect(shelf);
-	const highpass = input.context.createBiquadFilter();
-	highpass.type = "highpass";
-	highpass.frequency.value = 38;
-	highpass.Q.value = 0.5;
-	shelf.connect(highpass);
+	// const shelf = input.context.createBiquadFilter();
+	// shelf.type = "highshelf";
+	// shelf.frequency.value = 1500;
+	// shelf.gain.value = 4;
+	// input.connect(shelf);
+	// const highpass = input.context.createBiquadFilter();
+	// highpass.type = "highpass";
+	// highpass.frequency.value = 38;
+	// highpass.Q.value = 0.5;
+	// shelf.connect(highpass);
 	
 	console.log(workletUrl);
 	await input.context.audioWorklet.addModule(workletUrl);
@@ -26,9 +25,9 @@ export async function createLoudnessMeasurement(
 	processor.port.onmessage = evt => {
 		callback(evt.data);
 	}
-	highpass.connect(processor);
+	input.connect(processor);
 	return () => {
-		input.disconnect(shelf);
+		input.disconnect(processor);
 	}
 }
 
