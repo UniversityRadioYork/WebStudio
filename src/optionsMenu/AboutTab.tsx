d3ac2f720a1a8e3191b964b0e10ae21848cebcf6
Marks Polakovs <github@markspolakovs.me>
2020-04-12 10:31:02 +0200

    bit more debugging in the about page

--- src/optionsMenu/AboutTab.tsx
+++ src/optionsMenu/AboutTab.tsx
@@ -10,6 +10,7 @@ export function AboutTab() {
 			<div><b>WebStudio v{process.env.REACT_APP_VERSION}</b></div>
 			<div>MyRadio endpoint: <code>{MYRADIO_BASE_URL}</code>/<code>{MYRADIO_NON_API_BASE}</code></div>
 			<div>Streaming server: <code>{process.env.REACT_APP_WS_URL}</code></div>
+			<div>Broadcast control server: <code>{process.env.BROADCAST_API_BASE_URL}</code></div>
 			<div>Brought to you by URY Computing Team</div>
 		</>
 	);
