6ee50b198e9e3c4288b0454fdad65d5530902280
michael-grace <michael.dp.grace@outlook.com>
2020-04-22 22:33:41 +0100

    move mic meter

--- src/navbar/index.tsx
+++ src/navbar/index.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Clock from "react-live-clock";
-import { VUMeter } from "../optionsMenu/helpers/VUMeter";
 
 import { FaRegClock, FaRegUser } from "react-icons/fa";
 
@@ -68,15 +67,6 @@ export function NavBar() {
               timezone={"europe/london"}
             />
           </div>
-        </div>
-
-        <div className="nav-item" id="navMeter">
-          <VUMeter
-            width={400}
-            height={40}
-            range={[-70, 0]}
-            greenRange={[-14, -3]}
-          />
         </div>
       </div>
 
