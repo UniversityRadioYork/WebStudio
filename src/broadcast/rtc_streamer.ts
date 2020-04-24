60ddc1fc89c9f354feaaee82c18a965a53bb1369
Marks Polakovs <github@markspolakovs.me>
2020-04-24 11:00:14 +0200

    Add additional STUN servers

--- src/broadcast/rtc_streamer.ts
+++ src/broadcast/rtc_streamer.ts
@@ -32,6 +32,15 @@ export class WebRTCStreamer extends Streamer {
     this.pc = new RTCPeerConnection({
       iceServers: [
         {
+          urls: [
+            "stun:stun.l.google.com:19302",
+            "stun:stun1.l.google.com:19302",
+            "stun:stun2.l.google.com:19302",
+            "stun:stun3.l.google.com:19302",
+            "stun:stun4.l.google.com:19302",
+          ],
+        },
+        {
           urls: ["stun:eu-turn4.xirsys.com"],
         },
         {
