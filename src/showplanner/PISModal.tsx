1d25dc95cb133ee2ab2411155ff9ee18c0ea258e
Marks Polakovs <marks@markspolakovs.me>
2020-09-19 14:11:49 +0100

    And make it pretty

--- src/showplanner/PISModal.tsx
+++ src/showplanner/PISModal.tsx
@@ -9,10 +9,19 @@ function DevWarning() {
   }
   return (
     <div style={{ color: "white", backgroundColor: "#960000" }}>
-    <h1>Development Version</h1>
-    <strong>You are using a development version of WebStudio.</strong> This version is NOT tested and may have severe bugs and performance problems.<br />
-    <em><strong>DO NOT BROADCAST LIVE SHOWS USING THIS VERSION!</strong></em><br />
-    For the latest and greatest tested WebStudio, go to <a href={process.env.REACT_APP_HOMEPAGE}>{process.env.REACT_APP_HOMEPAGE}</a>.
+      <h1>Development Version</h1>
+      <strong>You are using a development version of WebStudio.</strong> This
+      version is NOT tested and may have severe bugs and performance problems.
+      <br />
+      <em>
+        <strong>DO NOT BROADCAST LIVE SHOWS USING THIS VERSION!</strong>
+      </em>
+      <br />
+      For the latest and greatest tested WebStudio, go to{" "}
+      <a href={process.env.REACT_APP_HOMEPAGE}>
+        {process.env.REACT_APP_HOMEPAGE}
+      </a>
+      .
     </div>
   );
 }
