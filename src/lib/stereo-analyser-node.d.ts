ea95a347652d8340a780eab64da26201cd6f2130
Matthew Stratford <hi@mstratford.net>
2020-09-25 16:37:41 +0100

    Last prettier

--- src/lib/stereo-analyser-node.d.ts
+++ src/lib/stereo-analyser-node.d.ts
@@ -1,22 +1,21 @@
 declare module "stereo-analyser-node" {
-
-declare interface StereoAnalyserNode {
-  StereoAnalyserNode(
-    audioContext: AudioContext,
-    opts: Object | undefined
-  ): {
-    fftSize: number;
-    frequencyBinCount: number;
-    minDecibels: number;
-    maxDecibels: number;
-    smoothingTimeConstant: number;
-    connect(destination: AudioNode | AudioParam): void;
-    disconnect(): void;
-    getFloatFrequencyData(arrayL: Float32Array, arrayR: Float32Array): void;
-    getByteFrequencyData(arrayL: Uint8Array, arrayR: Uint8Array): void;
-    getFloatTimeDomainData(arrayL: Float32Array, arrayR: Float32Array): void;
-    getByteTimeDomainData(arrayL: Uint8Array, arrayR: Uint8Array): void;
+  declare interface StereoAnalyserNode {
+    StereoAnalyserNode(
+      audioContext: AudioContext,
+      opts: Object | undefined
+    ): {
+      fftSize: number;
+      frequencyBinCount: number;
+      minDecibels: number;
+      maxDecibels: number;
+      smoothingTimeConstant: number;
+      connect(destination: AudioNode | AudioParam): void;
+      disconnect(): void;
+      getFloatFrequencyData(arrayL: Float32Array, arrayR: Float32Array): void;
+      getByteFrequencyData(arrayL: Uint8Array, arrayR: Uint8Array): void;
+      getFloatTimeDomainData(arrayL: Float32Array, arrayR: Float32Array): void;
+      getByteTimeDomainData(arrayL: Uint8Array, arrayR: Uint8Array): void;
+    };
   }
-}
-export default StereoAnalyserNode.StereoAnalyserNode();
+  export default StereoAnalyserNode.StereoAnalyserNode();
 }
