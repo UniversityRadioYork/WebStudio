c49550f2a54e7b628a343f2c3b8a4a2b8765d191
Matthew Stratford <hi@mstratford.net>
2020-09-25 13:42:25 +0100

    Typescript Definition for StereoAnalyserNode

--- src/lib/stereo-analyser-node.d.ts
+++ src/lib/stereo-analyser-node.d.ts
@@ -1,4 +1,22 @@
-declare module "stereo-analyser-node";
+declare module "stereo-analyser-node" {
 
-//export function StereoAnalyserNode(audioContext: AudioContext, opts);
-//export function getFloatTimeDomainData(array: Float32Array);
+declare interface StereoAnalyserNode {
+  StereoAnalyserNode(
+    audioContext: AudioContext,
+    opts: Object | undefined
+  ): {
+    fftSize: number;
+    frequencyBinCount: number;
+    minDecibels: number;
+    maxDecibels: number;
+    smoothingTimeConstant: number;
+    connect(destination: AudioNode | AudioParam): void;
+    disconnect(): void;
+    getFloatFrequencyData(arrayL: Float32Array, arrayR: Float32Array): void;
+    getByteFrequencyData(arrayL: Uint8Array, arrayR: Uint8Array): void;
+    getFloatTimeDomainData(arrayL: Float32Array, arrayR: Float32Array): void;
+    getByteTimeDomainData(arrayL: Uint8Array, arrayR: Uint8Array): void;
+  }
+}
+export default StereoAnalyserNode.StereoAnalyserNode();
+}
