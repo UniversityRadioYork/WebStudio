74f6b9c0ba1468399ec656f4a968e0168c46f6ce
Matthew Stratford <hi@mstratford.net>
2020-10-02 19:57:45 +0100

    Make WebStudio close the import window.

--- src/showplanner/ImporterModal.tsx
+++ src/showplanner/ImporterModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FaTimes, FaFileImport } from "react-icons/fa";
 import Modal from "react-modal";
 import { Button } from "reactstrap";
@@ -11,6 +11,18 @@ interface ImporterProps {
 // TODO: This needs updating to actually either provide the weighting channel values (less preferred)
 // or update the importer to work that out itself.
 export function ImporterModal(props: ImporterProps) {
+  // Add support for closing the modal when the importer wants to reload the show plan.
+  useEffect(() => {
+    window.addEventListener(
+      "message",
+      (event) => {
+        if (event.data === "reload_showplan") {
+          props.close();
+        }
+      },
+      false
+    );
+  });
   return (
     <Modal isOpen={props.isOpen} onRequestClose={props.close}>
       <div>
--- src/showplanner/index.tsx
+++ src/showplanner/index.tsx
@@ -372,17 +372,18 @@ const Showplanner: React.FC<{ timeslotId: number }> = function({ timeslotId }) {
   }
 
   // Add support for reloading the show plan from the iFrames.
-  window.addEventListener(
-    "message",
-    (event) => {
-      if (event.data === "reload_showplan") {
-        session.currentTimeslot !== null &&
-          dispatch(getShowplan(session.currentTimeslot.timeslot_id));
-      }
-    },
-    false
-  );
-
+  useEffect(() => {
+    window.addEventListener(
+      "message",
+      (event) => {
+        if (event.data === "reload_showplan") {
+          session.currentTimeslot !== null &&
+            dispatch(getShowplan(session.currentTimeslot.timeslot_id));
+        }
+      },
+      false
+    );
+  });
   if (showplan === null) {
     return (
       <LoadingDialogue
