8fc39db5c072d15354389c50dab7efb964a16b93
Marks Polakovs <marks@markspolakovs.me>
2020-10-06 13:29:06 +0100

    Save limited action history for Raygun reports

--- src/store.ts
+++ src/store.ts
@@ -23,12 +23,23 @@ import {
   REGISTER,
 } from "redux-persist";
 
-const raygunMiddleware: Middleware<{}, RootState, Dispatch<any>> = (store) => (
-  next
-) => (action) => {
-  raygun("recordBreadcrumb", "redux-action", action);
+const ACTION_HISTORY_MAX_SIZE = 20;
+
+const actionHistory: Array<Action> = [];
+
+const actionHistoryMiddleware: Middleware<{}, RootState, Dispatch<any>> = (
+  store
+) => (next) => (action) => {
+  while (actionHistory.length > ACTION_HISTORY_MAX_SIZE) {
+    actionHistory.shift();
+  }
+  actionHistory.push(action);
   return next(action);
 };
+
+export function getActionHistory() {
+  return actionHistory;
+}
 
 // See https://github.com/rt2zz/redux-persist/issues/988 for getDefaultMiddleware tweak.
 const store = configureStore({
@@ -36,7 +47,7 @@ const store = configureStore({
   middleware: [
     mixerMiddleware,
     mixerKeyboardShortcutsMiddleware,
-    raygunMiddleware,
+    actionHistoryMiddleware,
     ...getDefaultMiddleware({
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
