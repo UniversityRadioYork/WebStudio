0a3e2fa892f0c6559525d4c4eebbae91b81a0302
michael-grace <michael.dp.grace@outlook.com>
2020-10-19 00:34:16 +0100

    end of recording popup

---
+++ src/navbar/FinishRecordingModal.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { FaTimes, FaMicrophone } from "react-icons/fa";
+import Modal from "react-modal";
+import { Button } from "reactstrap";
+
+interface FinishRecordingModal {
+  isOpen: boolean;
+  close: () => any;
+}
+
+export function FinishRecordingModal(props: FinishRecordingModal) {
+  return (
+    <Modal isOpen={props.isOpen} onRequestClose={props.close}>
+      <h1 className="d-inline">
+        <FaMicrophone className="mx-2" size={30} />
+        Recording Complete
+      </h1>
+      <Button
+        onClick={props.close}
+        className="float-right pt-1"
+        color="primary"
+      >
+        <FaTimes />
+      </Button>
+      <hr className="mt-1 mb-3" />
+      <p>
+        Your recording is now complete. You can listen to it back and then
+        download it, add it to your show plan, or delete it by closing this
+        window.
+      </p>
+    </Modal>
+  );
+}
