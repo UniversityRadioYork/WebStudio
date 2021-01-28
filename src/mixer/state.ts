f5827f346e955c604ebf41d54c5d687035a0afd0
Matthew Stratford <hi@mstratford.net>
2021-01-28 02:36:43 +0000

    Update keyboard shortcuts for PFL channel.

--- src/mixer/state.ts
+++ src/mixer/state.ts
@@ -973,59 +973,59 @@ export const mixerKeyboardShortcutsMiddleware: Middleware<
   Dispatch<any>
 > = (store) => {
   Keys("q", () => {
-    store.dispatch(play(0));
-  });
-  Keys("w", () => {
-    store.dispatch(pause(0));
-  });
-  Keys("e", () => {
-    store.dispatch(stop(0));
-  });
-  Keys("r", () => {
     store.dispatch(play(1));
   });
-  Keys("t", () => {
+  Keys("w", () => {
     store.dispatch(pause(1));
   });
-  Keys("y", () => {
+  Keys("e", () => {
     store.dispatch(stop(1));
   });
-  Keys("u", () => {
+  Keys("r", () => {
     store.dispatch(play(2));
   });
-  Keys("i", () => {
+  Keys("t", () => {
     store.dispatch(pause(2));
   });
-  Keys("o", () => {
+  Keys("y", () => {
     store.dispatch(stop(2));
+  });
+  Keys("u", () => {
+    store.dispatch(play(3));
+  });
+  Keys("i", () => {
+    store.dispatch(pause(3));
+  });
+  Keys("o", () => {
+    store.dispatch(stop(3));
   });
 
   Keys("a", () => {
-    store.dispatch(setVolume(0, "off"));
-  });
-  Keys("s", () => {
-    store.dispatch(setVolume(0, "bed"));
-  });
-  Keys("d", () => {
-    store.dispatch(setVolume(0, "full"));
-  });
-  Keys("f", () => {
     store.dispatch(setVolume(1, "off"));
   });
-  Keys("g", () => {
+  Keys("s", () => {
     store.dispatch(setVolume(1, "bed"));
   });
-  Keys("h", () => {
+  Keys("d", () => {
     store.dispatch(setVolume(1, "full"));
   });
-  Keys("j", () => {
+  Keys("f", () => {
     store.dispatch(setVolume(2, "off"));
   });
-  Keys("k", () => {
+  Keys("g", () => {
     store.dispatch(setVolume(2, "bed"));
   });
-  Keys("l", () => {
+  Keys("h", () => {
     store.dispatch(setVolume(2, "full"));
+  });
+  Keys("j", () => {
+    store.dispatch(setVolume(3, "off"));
+  });
+  Keys("k", () => {
+    store.dispatch(setVolume(3, "bed"));
+  });
+  Keys("l", () => {
+    store.dispatch(setVolume(3, "full"));
   });
 
   Keys("x", () => {
