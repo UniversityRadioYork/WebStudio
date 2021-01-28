44c91aeea1ef53a8463ba5b0ead61b3cdb21624d
Matthew Stratford <hi@mstratford.net>
2021-01-28 22:19:32 +0000

    Add on air source to diddy timelord.

--- src/navbar/timelord.tsx
+++ src/navbar/timelord.tsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import LiveClock from "react-live-clock";
+import { myradioApiRequest } from "../api";
+import { useInterval } from "../lib/utils";
 import "./timelord.scss";
 
 export function Timelord() {
+  async function getSource() {
+    let studio = await myradioApiRequest("/selector/studioattime", "GET", null);
+
+    const sourceNames = [
+      "Studio Red",
+      "Studio Blue",
+      "Jukebox",
+      "OB",
+      "WebStudio",
+      "Unknown",
+      "Sine Wave",
+      "Off Air",
+    ];
+
+    let sourceName = "Unknown";
+    if (sourceNames[studio - 1]) {
+      sourceName = sourceNames[studio - 1];
+    }
+
+    return { id: studio, name: sourceName };
+  }
+
+  const [source, setSource] = useState({ id: 0, name: "Unknown" });
+
+  useInterval(async () => {
+    setSource(await getSource());
+  }, 3000);
+
   return (
     <li
       className="btn rounded-0 py-2 nav-link nav-item"
@@ -21,6 +51,10 @@ export function Timelord() {
         ticking={true}
         timezone={"europe/london"}
       />
+      <span className="source">
+        <span className={"studio studio" + source.id}>{source.name}</span> is On
+        Air
+      </span>
     </li>
   );
 }
