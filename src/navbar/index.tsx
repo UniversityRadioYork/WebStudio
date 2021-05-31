a6cb679656864aa65d57201f7a5318eb6dfa59a8
Matthew Stratford <hi@mstratford.net>
2021-05-31 21:35:58 +0100

    Update Navbar

--- src/navbar/index.tsx
+++ src/navbar/index.tsx
@@ -1,24 +1,241 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Clock from "react-live-clock";
+import Stopwatch from "react-stopwatch";
 
-import { FaCompactDisc } from "react-icons/fa";
+import {
+  FaCircle,
+  FaRegClock,
+  FaRegUser,
+  FaBroadcastTower,
+  FaSpinner,
+  FaExclamationTriangle,
+  FaCog,
+  FaCompactDisc,
+} from "react-icons/fa";
 
 import { RootState } from "../rootReducer";
 
+import * as BroadcastState from "../broadcast/state";
+import appLogo from "../assets/images/webstudio.svg";
+import myradioLogo from "../assets/images/myradio.svg";
+import { MYRADIO_NON_API_BASE } from "../api";
 import logo from "../assets/images/navbarlogo.png";
 import "./navbar.scss";
 import { closeAlert } from "./state";
 import { BAPSicleModal } from "./BAPSicleModal";
 
+import { ConnectionStateEnum } from "../broadcast/streamer";
+import { VUMeter } from "../optionsMenu/helpers/VUMeter";
+import { getShowplan, setItemPlayed } from "../showplanner/state";
+
+import * as OptionsMenuState from "../optionsMenu/state";
+
+function nicifyConnectionState(state: ConnectionStateEnum): string {
+  switch (state) {
+    case "CONNECTED":
+      return "Connected!";
+    case "CONNECTING":
+      return "Connecting to server...";
+    case "CONNECTION_LOST":
+      return "Connection lost!";
+    case "CONNECTION_LOST_RECONNECTING":
+      return "Connection lost. Reconnecting...";
+    case "NOT_CONNECTED":
+      return "Not Connected";
+    case "LIVE":
+      return "Live!";
+    default:
+      console.warn("unhandled", state);
+      return state as string;
+  }
+}
+
+export function NavBarMyRadio() {
+  const dispatch = useDispatch();
+  const sessionState = useSelector((state: RootState) => state.session);
+  const redirect_url = encodeURIComponent(window.location.toString());
+
+  return (
+    <>
+      <div className="navbar-nav navbar-left">
+        <a className="navbar-brand" href="/">
+          <img
+            src="//ury.org.uk/myradio/img/URY.svg"
+            height="30"
+            alt="University Radio York Logo"
+          />
+        </a>
+        <span className="navbar-brand divider"></span>
+        <a
+          className="navbar-brand logo-hover"
+          href={MYRADIO_NON_API_BASE}
+          title="Back to MyRadio"
+        >
+          <img className="logo-webstudio" src={appLogo} alt="Web Studio Logo" />
+          <img className="logo-myradio" src={myradioLogo} alt="MyRadio Logo" />
+        </a>
+      </div>
+
+      <ul className="nav navbar-nav navbar-right">
+        <li className="nav-item dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href={
+              MYRADIO_NON_API_BASE + "/MyRadio/timeslot/?next=" + redirect_url
+            }
+            id="timeslotDropdown"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <FaRegClock />
+            &nbsp;
+            {sessionState.currentTimeslot &&
+              sessionState.currentTimeslot.start_time}
+          </a>
+          <div className="dropdown-menu" aria-labelledby="timeslotDropdown">
+            <a
+              className="dropdown-item"
+              href={
+                MYRADIO_NON_API_BASE + "/MyRadio/timeslot/?next=" + redirect_url
+              }
+            >
+              Switch Timeslot
+            </a>
+            <button
+              className="dropdown-item"
+              onClick={() =>
+                sessionState.currentTimeslot !== null &&
+                dispatch(getShowplan(sessionState.currentTimeslot.timeslot_id))
+              }
+            >
+              Reload Show Plan
+            </button>
+            <button
+              className="dropdown-item"
+              onClick={() =>
+                sessionState.currentTimeslot !== null &&
+                dispatch(setItemPlayed({ itemId: "all", played: false }))
+              }
+            >
+              Mark All Items Unplayed
+            </button>
+            <h6 className="dropdown-header">
+              {sessionState.currentTimeslot?.title}
+            </h6>
+            <h6 className="dropdown-header">
+              ID: {sessionState.currentTimeslot?.timeslot_id}
+            </h6>
+          </div>
+        </li>
+        <li className="nav-item navbar-profile dropdown">
+          <a
+            className="nav-link dropdown-toggle"
+            href={MYRADIO_NON_API_BASE + "/Profile/default/"}
+            id="dropdown07"
+            data-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <FaRegUser />
+            &nbsp;
+            {sessionState.currentUser?.fname} {sessionState.currentUser?.sname}
+          </a>
+          <div className="dropdown-menu" aria-labelledby="dropdown07">
+            <a
+              className="dropdown-item"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={MYRADIO_NON_API_BASE + "/Profile/default/"}
+            >
+              My Profile
+            </a>
+            <a
+              className="dropdown-item"
+              href={MYRADIO_NON_API_BASE + "/MyRadio/logout/"}
+            >
+              Logout
+            </a>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+}
+
 export function NavBarMain() {
+  const dispatch = useDispatch();
+  const broadcastState = useSelector((state: RootState) => state.broadcast);
+  const settings = useSelector((state: RootState) => state.settings);
+
+  const [connectButtonAnimating, setConnectButtonAnimating] = useState(false);
+
+  const prevRegistrationStage = useRef(broadcastState.stage);
+  useEffect(() => {
+    if (broadcastState.stage !== prevRegistrationStage.current) {
+      setConnectButtonAnimating(false);
+    }
+    prevRegistrationStage.current = broadcastState.stage;
+  }, [broadcastState.stage]);
+
+  const { planSaveError, planSaving } = useSelector(
+    (state: RootState) => state.showplan
+  );
+
   const [showBAPSicleModal, setShowBAPSicleModal] = useState(true);
 
+  if (process.env.REACT_APP_BAPSICLE_INTERFACE) {
+    return (
+      <>
+        <ul className="nav navbar-nav navbar-left">
+          <li
+            className="btn rounded-0 py-1 nav-link nav-item"
+            id="timelord"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(
+                "http://ury.org.uk/timelord/",
+                "URY - Timelord",
+                "resizable,status"
+              );
+            }}
+          >
+            <img src={logo} className="mr-2" height={32} alt="Logo" />
+            <Clock
+              format={"HH:mm:ss"}
+              ticking={true}
+              timezone={"europe/london"}
+            />
+          </li>
+        </ul>
+
+        <div>
+          <ul className="nav navbar-nav navbar-right mr-0 pr-0">
+            <li
+              className="btn btn-outline-light rounded-0 pt-2 pb-2 nav-item nav-link"
+              style={{ color: "white" }}
+              onClick={() => {
+                setShowBAPSicleModal(true);
+              }}
+            >
+              <FaCompactDisc size={16} className="mr-2" />
+              <b>Menu</b>
+            </li>
+          </ul>
+        </div>
+        <BAPSicleModal
+          close={() => setShowBAPSicleModal(false)}
+          isOpen={showBAPSicleModal}
+        />
+      </>
+    );
+  }
+
+  // Full WebStudio NavBar
   return (
     <>
       <ul className="nav navbar-nav navbar-left">
         <li
-          className="btn rounded-0 py-1 nav-link nav-item"
+          className="btn rounded-0 py-2 nav-link nav-item"
           id="timelord"
           onClick={(e) => {
             e.preventDefault();
@@ -29,33 +246,113 @@ export function NavBarMain() {
             );
           }}
         >
-          <img src={logo} className="mr-2" height={32} alt="Logo" />
           <Clock
             format={"HH:mm:ss"}
             ticking={true}
             timezone={"europe/london"}
           />
         </li>
+        {planSaving && (
+          <li className="btn rounded-0 py-2 nav-item alert-info">
+            <FaSpinner className="nav-spin mb-1" /> Saving show plan...
+          </li>
+        )}
+        {planSaveError && (
+          <li className="btn rounded-0 py-2 nav-item alert-danger">
+            <FaExclamationTriangle className="p-0 mr-1" />
+            {planSaveError}
+          </li>
+        )}
       </ul>
 
-      <div>
-        <ul className="nav navbar-nav navbar-right mr-0 pr-0">
+      <ul className="nav navbar-nav navbar-right mr-0 pr-0">
+        <li className="nav-item" style={{ color: "white" }}>
+          <div className="nav-link">
+            <b>{nicifyConnectionState(broadcastState.connectionState)}</b>
+          </div>
+        </li>
+        <li
+          className="btn btn-outline-light rounded-0 pt-2 pb-1 nav-item nav-link connect"
+          onClick={() => {
+            setConnectButtonAnimating(true);
+            switch (broadcastState.stage) {
+              case "NOT_REGISTERED":
+                dispatch(BroadcastState.goOnAir());
+                break;
+              case "REGISTERED":
+                dispatch(BroadcastState.cancelTimeslot());
+                break;
+            }
+          }}
+        >
+          {connectButtonAnimating ? (
+            <>
+              <FaBroadcastTower size={17} className="mr-2" />
+              <FaSpinner size={17} className="nav-spin mr-2" />
+            </>
+          ) : (
+            <>
+              <FaBroadcastTower size={17} className="mr-2" />
+              {broadcastState.stage === "NOT_REGISTERED" && "Register"}
+              {broadcastState.stage === "REGISTERED" && "Stop"}
+            </>
+          )}
+        </li>
+        {settings.enableRecording && (
           <li
-            className="btn btn-outline-light rounded-0 pt-2 pb-2 nav-item nav-link"
-            style={{ color: "white" }}
-            onClick={() => {
-              setShowBAPSicleModal(true);
-            }}
+            className={
+              "btn rounded-0 pt-2 pb-1 nav-item nav-link " +
+              (broadcastState.recordingState === "CONNECTED"
+                ? "btn-outline-danger active"
+                : "btn-outline-light")
+            }
+            onClick={() =>
+              dispatch(
+                broadcastState.recordingState === "NOT_CONNECTED"
+                  ? BroadcastState.startRecording()
+                  : BroadcastState.stopRecording()
+              )
+            }
           >
-            <FaCompactDisc size={16} className="mr-2" />
-            <b>Menu</b>
+            <FaCircle
+              size={17}
+              className={
+                broadcastState.recordingState === "CONNECTED"
+                  ? "rec-blink"
+                  : "rec-stop"
+              }
+            />{" "}
+            {broadcastState.recordingState === "CONNECTED" ? (
+              <Stopwatch
+                seconds={0}
+                minutes={0}
+                hours={0}
+                render={({ formatted }) => {
+                  return <span>{formatted}</span>;
+                }}
+              />
+            ) : (
+              "Record"
+            )}
           </li>
-        </ul>
-      </div>
-      <BAPSicleModal
-        close={() => setShowBAPSicleModal(false)}
-        isOpen={showBAPSicleModal}
-      />
+        )}
+        <li
+          className="btn btn-outline-light rounded-0 pt-2 pb-1 nav-item nav-link"
+          onClick={() => dispatch(OptionsMenuState.open())}
+        >
+          <FaCog size={17} /> Options
+        </li>
+
+        <li className="nav-item px-2 nav-vu">
+          <VUMeter
+            width={235}
+            height={40}
+            source="master"
+            range={[-40, 3]}
+            stereo={true}
+          />
+        </li>
+      </ul>
     </>
   );
 }
@@ -98,6 +395,18 @@ export function CombinedNavAlertBar() {
   return (
     <>
       <AlertBar />
+      (!process.env.REACT_APP_BAPSICLE_INTERFACE &&
+      <>
+        <header className="navbar navbar-myradio navbar-expand-sm hover-menu p-0 bd-navbar">
+          <span className="hover-label hover-label-hide text-light text-center">
+            Hover for MyRadio Menu
+          </span>
+          <nav className="container-fluid px-0">
+            <NavBarMyRadio />
+          </nav>
+        </header>
+      </>
+      )
       <header className="navbar navbar-main navbar-expand-sm p-0 bd-navbar">
         <nav className="container-fluid px-0">
           <NavBarMain />
