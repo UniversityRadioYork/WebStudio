3b5677e4824c8365492a0132037fe60d7e43932e
Matthew Stratford <hi@mstratford.net>
2021-09-11 19:15:08 +0100

    Delete the other old wav import.

--- src/mixer/audio.ts
+++ src/mixer/audio.ts
@@ -4,7 +4,6 @@ import StrictEmitter from "strict-event-emitter-types";
 import WaveSurfer from "wavesurfer.js";
 import CursorPlugin from "wavesurfer.js/dist/plugin/wavesurfer.cursor.min.js";
 import RegionsPlugin from "wavesurfer.js/dist/plugin/wavesurfer.regions.min.js";
-import NewsEndCountdown from "../assets/audio/NewsEndCountdown.wav";
 import NewsIntro from "../assets/audio/NewsIntro.wav";
 
 import StereoAnalyserNode from "stereo-analyser-node";
