95d317f43c94978c45b9e4ba61cbc5eaeddde15a
Matthew Stratford <hi@mstratford.net>
2023-04-14 23:40:06 +0100

    Revert "Linting"
    This reverts commit db5c76489a2a73cf7ee02ab1f123b8d4a31a32a1.

--- scripts/start.js
+++ scripts/start.js
@@ -132,8 +132,8 @@ checkBrowsers(paths.appPath, isInteractive)
       openBrowser(urls.localUrlForBrowser);
     });
 
-    ["SIGINT", "SIGTERM"].forEach(function (sig) {
-      process.on(sig, function () {
+    ["SIGINT", "SIGTERM"].forEach(function(sig) {
+      process.on(sig, function() {
         devServer.close();
         process.exit();
       });
