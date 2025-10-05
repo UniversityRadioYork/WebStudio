3fa94e0a972dfc45e1df52ade93df8617e7f22eb
Matthew Stratford <hi@mstratford.net>
2025-10-05 16:10:42 +0100

    Fix ID in popup is null when trackId is present from baps but null.

--- src/showplanner/Item.tsx
+++ src/showplanner/Item.tsx
@@ -167,7 +167,7 @@ export const Item = memo(function Item({
       );
     }
     data.push(
-      "ID: " + ("trackid" in x ? x.trackid : "managedid" in x && x.managedid)
+      "ID: " + ("trackid" in x && x.trackid ? x.trackid : "managedid" in x && x.managedid ? x.managedid : "None" )
     );
     if (showDebug) {
       data.push(
